5fab2eac2c31eb86356c286c888441712fd435f4
jeremias <juansebas7011@gmail.com>
2025-02-04 17:57:10 -0500

    estoy miraando a ver como hago para ver el js

---
+++ array.js
@@ -0,0 +1,8 @@
+ let tareas= [];           //aqui primero se declara el array vacio
+
+ tareas.unshift("lavar ropa");
+ //aqui se agrega un elemento al principio del array
+
+ tareas.push("Hacer la compra");
+ 
+ const listaDeTareas = document.getElementById("lista-de-tareas");
